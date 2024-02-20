248058a5fe233ae401c455a76f9bb491e3270f69
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-02-20 21:33:30 +0700

    error pada bagian componenets

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,15 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Card() {
+export default function Card({ image, title, description }  ) {
   return (
-    <div className="" style={{ width: "18rem" }}>
-      <img src="src\assets\icon1.png" className="card-img-top" alt="..." style={{ width:80 }}/>
-      <div className="card-body">
-        <h5 className="card-title">Membership Organisations</h5>
-        <p className="card-text fs-6">
-        Our membership management software provides full automation of membership renewals and payments
-        </p>
-      </div>
+    <div className="d-flex row justify-content-center gap-2" style={{ width: "18rem" }}>
+      <img src={image} className="card-img-top" alt={image} style={{ width:80 }}/>
+      <h5 className="card-title fs-4 text-center">{title}</h5>
+      <p className="card-text fs-6 text-center">
+        {description}
+      </p>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+import icon1 from './assets/icon1'
+import icon2 from './assets/icon2'
+import icon3 from './assets/icon3'
+
+
+export const CARD_HERO=[
+    {
+        image: icon1,
+        title: 'Membership Organisations',
+        description : 'Our membership management software provides full automation of membership renewals and payments',
+    },
+    {
+        image: icon2,
+        title: 'National Associations',
+        description: 'Our membership management software provides full automation of membership renewals and payments',
+    },
+    {
+        image: icon3,
+        title: 'Clubs And Groups',
+        description: 'Our membership management software provides full automation of membership renewals and payments',
+    },
+]
